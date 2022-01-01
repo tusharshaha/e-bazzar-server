3159b2d5404f2c6449b4741896c7df30a91c87ab
tusharshaha <tusharshaha128@gmail.com>
2022-01-01 22:23:41 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xl13.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('E-Bazzar');
+        const productCollections = database.collection('products')
+        const orderCollections = database.collection('orders')
+
+        
+    }finally{
+        await client.close()
+    }
+}
+run().catch(console.dir)
 // verify server hitting
 app.get('/', (req, res) => {
     res.send('server hitting')
