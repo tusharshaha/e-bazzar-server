136a3909c161e92a423a847c14cab03856122af3
tusharshaha <tusharshaha128@gmail.com>
2022-01-02 14:41:35 +0600

    single product getting method added

--- index.js
+++ index.js
@@ -10,28 +10,37 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xl13.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-async function run(){
-    try{
+console.log(uri)
+async function run() {
+    try {
         await client.connect();
         const database = client.db('E-Bazzar');
         const productCollections = database.collection('products')
         const orderCollections = database.collection('orders')
 
+
         // get all products
-        app.get('/products', async (req,res)=>{
+        app.get('/products', async (req, res) => {
             const products = await productCollections.find({}).toArray()
             res.send(products)
         })
+        // get a single products
+        app.get('/products/:id',async (req,res)=>{
+            const productId = req.params.id;
+            const query = {_id: ObjectId(productId)}
+            const result = await productCollections.findOne(query);
+            res.send(result);
+        })
         // get an user order
-        app.get('/orders/:email', async(req,res)=>{
+        app.get('/orders/:email', async (req, res) => {
             const userEmail = req.params.email;
-            const query = {email: userEmail}
+            const query = { email: userEmail }
             const result = await orderCollections.find(query).toArray()
             res.send(result)
         })
-    }finally{
-        await client.close()
+
+    } finally {
+        // await client.close()
     }
 }
 run().catch(console.dir)
